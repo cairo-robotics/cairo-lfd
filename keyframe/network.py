c421b92cf65464184368012b50b6c5c17d18e523
Carl Mueller <cmuell89@gmail.com>
2018-01-11 23:18:24 -0700

    Start of keyframe node to be used for networkX

---
+++ keyframe/network.py
@@ -0,0 +1,20 @@
+
+
+class KeyframeNode:
+
+    def __init__(self, GMM, samples, keyframe_type="standard"):
+        self.gmm = GMM
+        self.samples = samples
+        self.keyframe_type = keyframe_type
+
+    def regenerate_samples(self):
+        self.samples = self.gmm.generate_samples()
+
+
+class WaypointGenerator:
+
+    def generate_way_points(self):
+        pass
+
+
+

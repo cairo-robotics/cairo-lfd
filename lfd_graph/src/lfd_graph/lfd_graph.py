9d3421787a6218edd2bd4dc56cff2e34d9679925
Jeff <jeve0658@colorado.edu>
2018-01-27 18:34:31 -0700

    work at home commit

--- lfd_graph/src/lfd_graph/lfd_graph.py
+++ lfd_graph/src/lfd_graph/lfd_graph.py
@@ -2,7 +2,9 @@
 
 
 import networkx as nx
+from keyframe.modeling import GaussianMixtureModel
 from keyframe.data_processing import DataImporter, DataProcessor
+
 #from keyframe.data_processing import DataImporter, DataProcessor
 
 import os, signal
@@ -10,23 +12,41 @@ import os, signal
 
 class TaskGraph(object):
 
-    def __init__(self, GMM, samples, keyframe_type="standard"):
+    def __init__(self):
         self.task_graph = nx.DiGraph()
+        self._processor = DataProcessor()
+        self._tail()
 
 
     def add_node(self, samples):
-        pass
+        observations = []
+        for observation in samples["data"]:
+            sample = self._processor.convert_observation_dict_to_list(observation)
+            observations.append(sample)
+
+        np_observations = self._processor.to_np_array(observations)
+        model = GaussianMixtureModel(np_observations)
+        model.gmm_fit()
+
 
     def check_sample_points(self):
         pass
 
 def main():
+
+    task_graph = TaskGraph()
+
     importer = DataImporter()
     processor = DataProcessor()
     dir = os.path.dirname(__file__)
-    filename = os.path.join(dir, "../../lfd_keyframe_examples/toy_data/keyframe_data/keyframes.json")
+    filename = os.path.join(dir, "../../../lfd_keyframe_examples/toy_data/keyframe_data/keyframes.json")
     keyframe_data = importer.load_json_files(filename)
-    
+
+    task_graph.add_node(keyframe_data['1'])
+
+    for key in sorted(keyframe_data.keys()):
+        pass
+
 
 
 

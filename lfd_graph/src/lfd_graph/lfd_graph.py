9e06d3c7f1e4a1890c8611d8b87f40ca95e5c537
Jeff <jeve0658@colorado.edu>
2018-01-28 18:57:36 -0700

    works clean it at home

--- lfd_graph/src/lfd_graph/lfd_graph.py
+++ lfd_graph/src/lfd_graph/lfd_graph.py
@@ -5,17 +5,53 @@ import networkx as nx
 from keyframe.modeling import GaussianMixtureModel
 from keyframe.data_processing import DataImporter, DataProcessor
 
+import os, signal
+
+import rospy
+import std_msgs.msg
+from std_msgs.msg import String
+import geometry_msgs.msg
+from geometry_msgs.msg import Pose
 #from keyframe.data_processing import DataImporter, DataProcessor
 
 import os, signal
 
+class DeathNote(object):
+    '''DeathNote for exiting runaway loops'''
+    write_name = False
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.ryuk)
+        signal.signal(signal.SIGTERM, self.ryuk)
+
+    def ryuk(self, signum, frame):
+        '''name written kill the loop'''
+        self.write_name = True
+
+Wait_flag = 0
+
+def callback(string):
+    global Wait_flag
+    Wait_flag = 0
+    print "completion callback"
+
+def node_to_pose(pose_data):
+    pose_msg = Pose()
+    pose_msg.orientation.z = pose_data[0][3]
+    pose_msg.orientation.x = pose_data[0][4]
+    pose_msg.orientation.y = pose_data[0][5]
+    pose_msg.orientation.w = pose_data[0][6]
+    pose_msg.position.x = pose_data[0][0]
+    pose_msg.position.y = pose_data[0][1]
+    pose_msg.position.z = pose_data[0][2]
+    return pose_msg
 
 class TaskGraph(object):
 
     def __init__(self):
         self.task_graph = nx.DiGraph()
         self._processor = DataProcessor()
-        self._tail()
+        self._head = None
+        self._tail = None
 
 
     def add_node(self, samples):
@@ -29,10 +65,40 @@ class TaskGraph(object):
         model.gmm_fit()
 
 
-    def check_sample_points(self):
+        new_pose = None
+        i = 0
+        while new_pose is None:
+            test_pose = model.generate_samples(1)
+            prior = model.predict_proba(test_pose)
+            if any(P > .95 for P in prior[0][:]):
+                pass
+            else:
+                new_pose = test_pose
+
+
+
+
+        if self._head is None:
+            self.task_graph.add_node(0, gmm=model, pose=new_pose)
+            self._head = 0
+            self._tail = 0
+        else:
+            self._tail += 1
+            self.task_graph.add_node(self._tail, gmm=model, pose=new_pose)
+            self.task_graph.add_edge(self._tail-1, self._tail)
+
+
+
+    def check_sample_points(self, point, gmm):
         pass
 
 def main():
+
+    pose_pub = rospy.Publisher("/commander/pose", Pose, queue_size=1)
+    sub_message = rospy.Subscriber("/test_topic", String, callback, queue_size=1)
+    rospy.init_node("graph_traverse")
+    print "hello world"
+    yagami = DeathNote()
 
     task_graph = TaskGraph()
 
@@ -42,10 +108,37 @@ def main():
     filename = os.path.join(dir, "../../../lfd_keyframe_examples/toy_data/keyframe_data/keyframes.json")
     keyframe_data = importer.load_json_files(filename)
 
-    task_graph.add_node(keyframe_data['1'])
-
     for key in sorted(keyframe_data.keys()):
-        pass
+        task_graph.add_node(keyframe_data[key])
+
+
+    global Wait_flag
+    Wait_flag = 1
+    current_node = 0
+
+    pose_data = node_to_pose(task_graph.task_graph.nodes[current_node]["pose"])
+
+    print pose_data
+    pose_pub.publish(pose_data)
+    global Wait_flag
+    Wait_flag = 1
+
+    while list(task_graph.task_graph.successors(current_node)):
+        if yagami.write_name: #execute death note
+            break
+        temp = list(task_graph.task_graph.successors(current_node))
+        current_node = temp[0]
+        print current_node
+        while(Wait_flag):
+            if yagami.write_name: #execute death note
+                break
+            pass
+        Wait_flag = 1
+        pose_data = node_to_pose(task_graph.task_graph.nodes[current_node]["pose"])
+        print pose_data
+        pose_pub.publish(pose_data)
+
+    #print prob
 
 
 

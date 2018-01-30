8e56d2a7454f27c30a7921115df4dc77f1692fac
Jeff <jeve0658@colorado.edu>
2018-01-29 18:51:22 -0700

    inheritance add n samples to node

--- lfd_graph/src/lfd_graph/lfd_graph.py
+++ lfd_graph/src/lfd_graph/lfd_graph.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-
-
+import numpy as np
 import networkx as nx
+from networkx import MultiDiGraph
 from keyframe.modeling import GaussianMixtureModel
 from keyframe.data_processing import DataImporter, DataProcessor
 
@@ -45,16 +45,16 @@ def node_to_pose(pose_data):
     pose_msg.position.z = pose_data[0][2]
     return pose_msg
 
-class TaskGraph(object):
+class TaskGraph(MultiDiGraph):
 
     def __init__(self):
-        self.task_graph = nx.DiGraph()
+        MultiDiGraph.__init__(self)
         self._processor = DataProcessor()
         self._head = None
         self._tail = None
 
 
-    def add_node(self, samples):
+    def add_gmm_node(self, samples):
         observations = []
         for observation in samples["data"]:
             sample = self._processor.convert_observation_dict_to_list(observation)
@@ -64,33 +64,35 @@ class TaskGraph(object):
         model = GaussianMixtureModel(np_observations)
         model.gmm_fit()
 
-
-        new_pose = None
-        i = 0
-        while new_pose is None:
-            test_pose = model.generate_samples(1)
-            prior = model.predict_proba(test_pose)
-            if any(P > .95 for P in prior[0][:]):
-                pass
-            else:
-                new_pose = test_pose
-
-
-
-
         if self._head is None:
-            self.task_graph.add_node(0, gmm=model, pose=new_pose)
+            self.add_node(0, gmm=model)
             self._head = 0
             self._tail = 0
         else:
             self._tail += 1
-            self.task_graph.add_node(self._tail, gmm=model, pose=new_pose)
-            self.task_graph.add_edge(self._tail-1, self._tail)
+            self.add_node(self._tail, gmm=model)
+            self.add_edge(self._tail-1, self._tail)
+
+
+
+    def add_n_samples_to_node(self, n, node):
+
+        sample = self.nodes[ node]['gmm'].generate_samples(1)
+        self.nodes[node]['points'].append(sample)
 
 
 
     def check_sample_points(self, point, gmm):
-        pass
+        '''
+        new_pose = None
+        i = 0
+        while new_pose is None:
+            pass
+            if any(P > .95 for P in prior[0][:]):
+                pass
+            else:
+                new_pose = test_pose
+                '''
 
 def main():
 
@@ -102,16 +104,19 @@ def main():
 
     task_graph = TaskGraph()
 
-    importer = DataImporter()
+    importer = Data Importer()
     processor = DataProcessor()
     dir = os.path.dirname(__file__)
     filename = os.path.join(dir, "../../../lfd_keyframe_examples/toy_data/keyframe_data/keyframes.json")
     keyframe_data = importer.load_json_files(filename)
 
     for key in sorted(keyframe_data.keys()):
-        task_graph.add_node(keyframe_data[key])
+        task_graph.add_gmm_node(keyframe_data[key])
 
+    task_graph.add_sample_to_node(1)
 
+    print task_graph.nodes[1]['points']
+    '''
     global Wait_flag
     Wait_flag = 1
     current_node = 0
@@ -140,7 +145,7 @@ def main():
 
     #print prob
 
-
+    '''
 
 
     print

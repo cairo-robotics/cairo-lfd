ec033fa2e91bc4b07d7fa40479109906b73c90d7
Jeff <jeve0658@colorado.edu>
2018-02-11 21:33:06 -0700

    renamed task graph

--- lfd_modeling/src/lfd_modeling/task_graph.py
+++ lfd_modeling/src/lfd_modeling/task_graph.py
@@ -2,19 +2,21 @@
 import numpy as np
 import networkx as nx
 from networkx import MultiDiGraph
-from keyframe.modeling import GaussianMixtureModel
-from keyframe.data_processing import DataImporter, DataProcessor
+
+from lfd_modeling.modeling import GaussianMixtureModel
+from lfd_processor.data_io import DataImporter
 
 import os, signal
 
 import rospy
+import rospkg
 import std_msgs.msg
 from std_msgs.msg import String
 import geometry_msgs.msg
 from geometry_msgs.msg import Pose
-#from keyframe.data_processing import DataImporter, DataProcessor
 
 import os, signal
+
 
 class DeathNote(object):
     '''DeathNote for exiting runaway loops'''
@@ -27,34 +29,20 @@ class DeathNote(object):
         '''name written kill the loop'''
         self.write_name = True
 
-Wait_flag = 0
 
-def callback(string):
-    global Wait_flag
-    Wait_flag = 0
-    print "completion callback"
-
-def node_to_pose(pose_data):
-    pose_msg = Pose()
-    pose_msg.orientation.z = pose_data[0][3]
-    pose_msg.orientation.x = pose_data[0][4]
-    pose_msg.orientation.y = pose_data[0][5]
-    pose_msg.orientation.w = pose_data[0][6]
-    pose_msg.position.x = pose_data[0][0]
-    pose_msg.position.y = pose_data[0][1]
-    pose_msg.position.z = pose_data[0][2]
-    return pose_msg
 
 class TaskGraph(MultiDiGraph):
 
     def __init__(self):
         MultiDiGraph.__init__(self)
-        self._processor = DataProcessor()
+        #self._processor = DataProcessor()
         self._head = None
         self._tail = None
 
 
     def add_gmm_node(self, samples):
+        pass
+        '''
         observations = []
         for observation in samples["data"]:
             sample = self._processor.convert_observation_dict_to_list(observation)
@@ -72,17 +60,19 @@ class TaskGraph(MultiDiGraph):
             self._tail += 1
             self.add_node(self._tail, gmm=model)
             self.add_edge(self._tail-1, self._tail)
-
+        '''
 
 
     def add_n_samples_to_node(self, n, node):
-
+        pass
+        '''
         sample = self.nodes[ node]['gmm'].generate_samples(1)
         self.nodes[node]['points'].append(sample)
-
+        '''
 
 
     def check_sample_points(self, point, gmm):
+        pass
         '''
         new_pose = None
         i = 0
@@ -95,28 +85,39 @@ class TaskGraph(MultiDiGraph):
                 '''
 
 def main():
-
-    pose_pub = rospy.Publisher("/commander/pose", Pose, queue_size=1)
-    sub_message = rospy.Subscriber("/test_topic", String, callback, queue_size=1)
-    rospy.init_node("graph_traverse")
-    print "hello world"
     yagami = DeathNote()
 
+    pose_pub = rospy.Publisher("/commander/pose", Pose, queue_size=1)
+    rospy.init_node("graph_traverse")
+    print "hello world"
+
     task_graph = TaskGraph()
+    importer = DataImporter()
 
-    importer = Data Importer()
-    processor = DataProcessor()
-    dir = os.path.dirname(__file__)
-    filename = os.path.join(dir, "../../../lfd_keyframe_examples/toy_data/keyframe_data/keyframes.json")
-    keyframe_data = importer.load_json_files(filename)
+    rospack = rospkg.RosPack()
+    pkg_path = rospack.get_path('lfd_processor')
 
+    file_path = "/src/lfd_processor/"
+    traj_file = "trajectory2.json"
+
+
+
+
+    keyframe_data = importer.import_json_to_dict(pkg_path + file_path+ traj_file)
+
+    for key in keyframe_data["trajectories"]:
+        print len(key)
+
+    print keyframe_data["trajectories"][0][0]["time"]
+
+    '''
     for key in sorted(keyframe_data.keys()):
         task_graph.add_gmm_node(keyframe_data[key])
 
     task_graph.add_sample_to_node(1)
 
     print task_graph.nodes[1]['points']
-    '''
+
     global Wait_flag
     Wait_flag = 1
     current_node = 0

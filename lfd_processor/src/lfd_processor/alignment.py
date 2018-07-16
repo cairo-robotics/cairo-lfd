d08877371984de865c2ac7011246d393e9b4154f
Carl Mueller <cmuell89@gmail.com>
2018-07-16 15:32:15 -0600

    Using the correct implementation of FastDTW

--- lfd_processor/src/lfd_processor/alignment.py
+++ lfd_processor/src/lfd_processor/alignment.py
@@ -1,5 +1,5 @@
 import rospy
-from dtw import fastdtw
+from fastdtw import fastdtw
 from scipy.spatial.distance import euclidean
 import copy
 
@@ -54,16 +54,11 @@ class DemonstrationAligner(object):
     def align(self):
 
         """
-        This function executes three identical loops where each demonstration is iteratively aligned against a 
-        chosen reference demonstration. The reference demonstration performs an aggregation role, where it collects 
-        the applied constraints against the demosntrations to which it is repeatedly aligned. The loop is repeated 
-        as it forces a convergence onto a uniform constraint mapping.
 
-        Alignment is performed using the FastDTW algorithm.
+        Alignment is performed using the FastDTW algorithm. It first separates trajectories that are constraint
+        annotated, and aligns those first. Secondly, arbitrarily uses one of those trajectories as a reference
+        against which to align all the remainin trajectories captured during demonstrations. If there are no 
 
-        Before the demonstrations are returned, the function iterates through each demonstration's aligned 
-        observations to perform a deepcopy of each observation. This ensures that during keyframe labeling, 
-        multiple references do not conflict as in place mutation occurs on observation objects during labeling.
 
         Returns
         -------
@@ -73,26 +68,41 @@ class DemonstrationAligner(object):
         rospy.loginfo("Aligning demosntrations...")
         if not len(self.demonstrations) > 1:
             raise Exception("Error! You are attempting to align ONLY ONE OR ZERO demonstrations.")
-        self.demonstrations.sort(key = lambda d: len(d.observations))
-        reference_demo = self.demonstrations[0]
-        for curr_demo in self.demonstrations:
-            # first loop collects applied constraints into shortest demonstration as master reference
-            alignments = self._get_alignment(curr_demo, reference_demo)
-            curr_demo.aligned_observations = alignments["current"]
-            reference_demo.aligned_observations = alignments["reference"]
-        for curr_demo in self.demonstrations:
-            alignments = self._get_alignment(curr_demo, reference_demo)
-            curr_demo.aligned_observations = alignments["current"]
-            reference_demo.aligned_observations = alignments["reference"]
-        for curr_demo in self.demonstrations:
-            # By third loop, constraints have converged to an equivalent mapping.
-            # Intuitively it makes some sense as iteratively running DTW will
-            # converge onto some global alignment if a reference vector is always used.
-            alignments = self._get_alignment(curr_demo, reference_demo)
-            curr_demo.aligned_observations = alignments["current"]
-            reference_demo.aligned_observations = alignments["reference"]
+
         for demo in self.demonstrations:
-            demo.aligned_observations = self._deep_copy_observations(demo.aligned_observations)
+            demo.aligned_observations = self._deep_copy_observations(demo.observations)
+
+        constrained_demonstrations = [demo for demo in self.demonstrations if any([len(ob.data["applied_constraints"]) != 0  for ob in demo.observations])]
+
+        # Align constrained demosntrations first, else if there are none, align all the trajectories without considering
+        # constraints.
+        if len(constrained_demonstrations) > 0:
+            constrained_demonstrations.sort(key = lambda d: len(d.observations))
+            reference_demo = constrained_demonstrations[0]
+
+            # Align constrained demonstrations first to ensure equivalent constraint transition mappings.
+            while self._check_for_equivalent_constraint_transitions(constrained_demonstrations) is False:
+                for curr_demo in constrained_demonstrations:
+                    # first loop collects applied constraints into shortest demonstration as master reference
+                    alignments = self._get_alignment(curr_demo, reference_demo)
+                    curr_demo.aligned_observations = alignments["current"]
+                    reference_demo.aligned_observations = alignments["reference"]
+
+            # Realign until uniform constraint transition mappings across all demostrations
+            while self._check_for_equivalent_constraint_transitions(self.demonstrations) is False:
+                self.demonstrations.sort(key = lambda d: len(d.observations))
+                for curr_demo in self.demonstrations:
+                    alignments = self._get_alignment(curr_demo, reference_demo)
+                    curr_demo.aligned_observations = alignments["current"]
+                    reference_demo.aligned_observations = alignments["reference"]
+        else:
+            self.demonstrations.sort(key=lambda d: len(d.aligned_observations), reverse=True)
+            reference_demo = self.demonstrations[0]
+            for curr_demo in self.demonstrations:
+                alignments = self._get_alignment(curr_demo, reference_demo)
+                curr_demo.aligned_observations = alignments["current"]
+                reference_demo.aligned_observations = alignments["reference"]
+
         constraint_transitions = self._get_universal_constraint_transitions(self.demonstrations)
         return (self.demonstrations, constraint_transitions)
 
@@ -120,8 +130,8 @@ class DemonstrationAligner(object):
 
         demos = [current_demo, reference_demo]
         demo_vectors = [self.vectorize_func(demo) for demo in demos]
-        dist, cost, acc, path = fastdtw(demo_vectors[0], demo_vectors[1], dist=euclidean)
-        idx_pairs = zip(path[0].tolist(), path[1].tolist())
+        dist, idx_pairs = fastdtw(demo_vectors[0], demo_vectors[1], dist=euclidean)
+        # idx_pairs = zip(path[0].tolist(), path[1].tolist())
 
         current_aligned_observations = []
         reference_aligned_observations = []
@@ -160,12 +170,56 @@ class DemonstrationAligner(object):
             new_observations.append(copy.deepcopy(ob))
         return new_observations
 
+    def _get_applied_constraint_order(self, observations):
+
+        """
+        Returns the applied constraint order of a Demonstration's aligned observation list.
+
+        Returns
+        -------
+        constraint_order: list
+            List of list where each element is ordered by the sequence of the applied constraints and represents
+            the set of constraints applied.
+        """
+
+        constraint_order = []
+        curr = []
+        for ob in observations:
+            if curr != ob.data["applied_constraints"]:
+                constraint_order.append(ob.data["applied_constraints"])
+                curr = ob.data["applied_constraints"]
+        return constraint_order
+
+    def _check_for_equivalent_constraint_transitions(self, demonstrations):
+
+        """
+        Checks for euivalent constraint transitions across all demonstrations. This should
+        occur after alignment.
+
+        Parameters
+        ----------
+        demonstrations : list
+           Demosntrations with which to generate the universal constraint transition map.
+
+        Returns
+        -------
+        : boolean
+            Boolean value with True indicating equivalent constraint transitions
+        """
+
+        mappings = [self._get_applied_constraint_order(demo.observations) for demo in demonstrations]
+
+        if mappings[1:] == mappings[:-1]:
+            return True
+        else:
+            return False
+
     def _get_universal_constraint_transitions(self, demonstrations):
 
         """
-        Generates the universal constraint transition mapping for all demosntraionts.
+        Generates the universal constraint transition mapping for all ALIGNED demonstrations.
         Raises an exception if any of the demosntrations has a difference mapping than
-        the others.
+        the others or if observations do not have aligned_observations instance variable populated.
 
         Parameters
         ----------
@@ -177,8 +231,20 @@ class DemonstrationAligner(object):
         mapping: list
             The universal mapping of constraint transitions for all the demonstraionts.
         """
-        mappings = [demo.get_applied_constraint_order() for demo in demonstrations]
+        try:
+            mappings = [self._get_applied_constraint_order(demo.aligned_observations) for demo in demonstrations]
+        except AttributeError as e:
+            raise AlignmentException("_get_universal_constraint_transitions() can only align demos\
+                                     that have their aligned_observations instance variable populated")
         if mappings[1:] == mappings[:-1]:
             return mappings[0]
         else:
             raise Exception("Unequivalent constraint transition mappings!")
+
+class AlignmentException(Exception):
+
+    """Base class for exceptions in this module."""
+
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message = message

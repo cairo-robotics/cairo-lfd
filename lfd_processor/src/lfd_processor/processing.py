3bd5db2f24470bb785dc170dec7b78e5a7e22070
Carl Mueller <cmuell89@gmail.com>
2019-01-05 17:52:54 -0700

    Data conversion functions for use in the KeyframeSampler class

--- lfd_processor/src/lfd_processor/processing.py
+++ lfd_processor/src/lfd_processor/processing.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python2.7
-
-import math
 import numpy as np
 from geometry_msgs.msg import Pose
+from environment import Observation
 
 
 def convert_data_to_pose(position, orientation):
@@ -28,103 +26,44 @@ def convert_data_to_pose(position, orientation):
     return pose
 
 
-class DataProcessor(object):
+class SawyerSampleConverter(object):
 
-    """
-    TODO: RENAME TO A MORE SPECIFIC CLASS NAME.
-    Data prcoessing class with a variety of methods to support manipulating imported data.
-    """
+    def __init__(self, interface):
+        self.interface = interface
 
-    def fixed_length_sampler(self, entries, fixed_length=250):
-
+    def convert(self, sample, run_fk=True):
         """
-        Takes a list and returns a list of a fixed length evenly pulled from the original list.
+
 
         Parameters
         ----------
-        entries : list
-            Original list to reduce to a list of fixed length.
-        fixed_length : int
-            The fixed length.
+
 
         Returns
         -------
-        fl_entries : list
-           A list of entries of fixed length, evenly distributed from the original list.
-        """
-
-        fl_entries = []
-        for entry in entries:
-            if len(entry) >= fixed_length:
-                fl_entry = []
-                length = float(len(entry))
-                for i in range(fixed_length):
-                    fl_entry.append(entry[int(math.ceil(i * length / fixed_length))])
-                fl_entries.append(fl_entry)
-            else:
-                raise Exception("Fixed length is less than length of entry. \\"
-                                "Try reducing the fixed length of a trajectory/keyframe")
-        return fl_entries
-
-    def convert_observation_dict_to_list(self,
-                                         observation,
-                                         key_order=["PoseX", "PoseY", "PoseZ", "OrienX",
-                                                    "OrienY", "OrienZ", "OrienW", "time"]):
 
         """
-        ####################
-        #### DEPRECATED ####
-        ####################
-        Takes an observation represented as a dictionary and converts it into a list.
+        if run_fk is True:
+            pose = self.interface.get_end_effector_pose(sample.tolist())
+            if pose is not None:
+                sample = np.insert(sample, 0, pose.orientation.w, axis=0)
+                sample = np.insert(sample, 0, pose.orientation.z, axis=0)
+                sample = np.insert(sample, 0, pose.orientation.y, axis=0)
+                sample = np.insert(sample, 0, pose.orientation.x, axis=0)
+                sample = np.insert(sample, 0, pose.position.z, axis=0)
+                sample = np.insert(sample, 0, pose.position.y, axis=0)
 
-        Parameters
-        ----------
-        observation : dict
-            Dictionary representation of an observation.
-        key_order : list of string
-            A list of keys in the order that each element of the generated list will represent.
+        normalize = np.sqrt(sample[3]**2 + sample[4]**2 +
+                            sample[5]**2 + sample[6]**2)
+        sample[3] = sample[3] / normalize
+        sample[4] = sample[4] / normalize
+        sample[5] = sample[5] / normalize
+        sample[6] = sample[6] / normalize
 
-        Returns
-        -------
-        observation_list : list
-           A list representation of the observation dict.
-        """
+        if len(sample) > 7:
+            # If length > 7, we know there must be joint data, so creat Obs w/ joints.
+            obsv = Observation.init_samples(sample[0:3], sample[3:7], sample[7:14])
+        else:
+            obsv = Observation.init_samples(sample[0:3], sample[3:7], None)
+        return obsv
 
-        observation_list = []
-        for key in key_order:
-            observation_list.append(observation[key])
-        return observation_list
-
-    def to_np_array(self, sequence, type='f'):
-        """
-        Wrapper function converting a list to a numpy array.
-
-        Parameters
-        ----------
-        sequence : list
-            The list to convert.
-        type : string
-            The numpy type that the elements will be converted to in the array.
-
-        Returns
-        -------
-        : numpy.ndarray
-           Numpy array converted list.
-        """
-        return np.array(sequence, dtype=type)
-
-
-class ObservationConverter(object):
-    ####################
-    #### DEPRECATED ####
-    ####################
-    def generate_pose(self, object_state):
-        pose = Pose()
-        pose.position.x = object_state["PoseX"]
-        pose.position.y = object_state["PoseY"]
-        pose.position.z = object_state["PoseZ"]
-        pose.orientation.x = object_state["OrienX"]
-        pose.orientation.y = object_state["OrienY"]
-        pose.orientation.z = object_state["OrienX"]
-        pose.orientation.w = object_state["OrienW"]
-        return pose

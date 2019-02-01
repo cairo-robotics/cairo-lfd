37b9e402a62a2fcd4d47a56d6c749ef3bf1d72e2
Carl Mueller <cmuell89@gmail.com>
2019-01-31 23:34:33 -0700

    Relative velocity
    Relative velocity seems to be holding currently, This ideally should be unit tested rigoursly but you know... time crunch.. grad school.. phd coding standards....sigh.

--- cairo_lfd/src/lfd/processing.py
+++ cairo_lfd/src/lfd/processing.py
@@ -6,6 +6,7 @@ from geometry_msgs.msg import Pose
 from environment import Observation
 from scipy.spatial.distance import euclidean
 from itertools import combinations
+from pudb import set_trace
 
 
 def convert_data_to_pose(position, orientation):
@@ -141,11 +142,13 @@ class ObjectRelativeDataProcessor():
         self.robot_id = robot_id
 
     def generate_relative_data(self, observations):
-        for obsv in observations:
+        for idx, obsv in enumerate(observations):
             self.relative_distance(obsv)
+            prior_observations = self._retrieve_prior_observations(observations, idx, 4)
+            self.relative_velocity(obsv, prior_observations)
 
     def relative_distance(self, observation):
-        # relative to items
+        # between items and items & robot
         for item_id in self.item_ids:
             relative_distances = {}
             item_data = observation.get_item_data(item_id)
@@ -154,33 +157,113 @@ class ObjectRelativeDataProcessor():
                 if item_id != target_id:
                     target_position = observation.get_item_data(target_id)["position"]
                     relative_distances[target_id] = self._euclidean(item_position, target_position)
-            if item_id != self.robot_id:
-                robot_position = observation.get_robot_data()["position"]
-                relative_distances[self.robot_id] = self._euclidean(item_position, robot_position)
+            robot_position = observation.get_robot_data()["position"]
+            relative_distances[self.robot_id] = self._euclidean(item_position, robot_position)
             item_data["relative_distance"] = relative_distances
 
         # relative to robots
-
         relative_distances = {}
         robot_data = observation.get_robot_data()
         robot_position = robot_data["position"]
         for item_id in self.item_ids:
-            if item_id != self.robot_id:
-                item_position = observation.get_item_data(item_id)["position"]
-                relative_distances[item_id] = self._euclidean(robot_position, item_position)
+            item_position = observation.get_item_data(item_id)["position"]
+            relative_distances[item_id] = self._euclidean(robot_position, item_position)
         robot_data["relative_distance"] = relative_distances
 
-    def relative_velocity(self, obj1_window, obj2_window):
-        pass
+    def relative_velocity(self, curr_observation, prior_observations):
+        # between items and items & robot
+        curr_time = curr_observation.get_timestamp()
+        for item_id in self.item_ids:
+            relative_velocities = {}
+            item_data = curr_observation.get_item_data(item_id)
+            item_relative_distances = item_data["relative_distance"]
+            # Loop through other items not the current item and calculate relative velocity:
+            for target_id in self.item_ids + [self.robot_id]:
+                if item_id != target_id:
+                    distances = []
+                    timestamps = []
+                    curr_target_distance = item_relative_distances.get(target_id)
+                    for prior_ob in prior_observations:
+                        distances.append(prior_ob.get_item_data(item_id)["relative_distance"].get(target_id))
+                        timestamps.append(prior_ob.get_timestamp())
+                    distances.append(curr_target_distance)
+                    timestamps.append(curr_time)
+                    reversed(distances)
+                    reversed(timestamps)
+                    if len(prior_observations) == 0:
+                        relative_velocities[target_id] = 0
+                    else:
+                        relative_velocities[target_id] = self._average_discrete_velocity(distances, timestamps)
+            item_data["relative_velocity"] = relative_velocities
+
+        # Now relative to robot, by accessing robot data separately.
+        relative_velocities = {}
+        robot_data = curr_observation.get_robot_data()
+        item_relative_distances = robot_data["relative_distance"]
+        for item_id in self.item_ids:
+            distances = []
+            timestamps = []
+            curr_target_distance = item_relative_distances.get(item_id)
+            for prior_ob in prior_observations:
+                distances.append(prior_ob.get_robot_data()["relative_distance"].get(item_id))
+                timestamps.append(prior_ob.get_timestamp())
+            distances.append(curr_target_distance)
+            timestamps.append(curr_time)
+            reversed(distances)
+            reversed(timestamps)
+            if len(prior_observations) == 0:
+                relative_velocities[item_id] = 0
+            else:
+                relative_velocities[item_id] = self._average_discrete_velocity(distances, timestamps)
+        robot_data["relative_velocity"] = relative_velocities
 
     def relative_acceleration(self, obj1_window, obj2_window):
         pass
 
-    def _get_window(self):
-        pass  
+    def _retrieve_prior_observations(self, observations, idx, number=1):
+        """
+        Retrieves a number of observations before given index. The number shrinks if the index would make number of elements reach out of the sequence index bounds.
+
+        Parameters
+        ----------
+
+        observations : list
+            List of Observation objects.
+
+        idx : int
+            The index of the current observation.
+
+        number : int
+            Number of prior observations.
+
+        Returns
+        -------
+        : list
+            List of prior Observation objects preceding the current observation.
+        """
+        if idx > len(observations) - 1:
+            raise IndexError("Index not valid for given length of observations sequence.")
+        for spread in reversed(range(number + 1)):
+            if 0 <= idx - spread < len(observations):
+                return observations[idx - spread:idx]
+        return []
 
     def _euclidean(self, obj1_posistion, obj2_position):
         if isinstance(obj1_posistion, (list, np.ndarray)) and isinstance(obj2_position, (list, np.ndarray)):
             return euclidean(obj1_posistion, obj2_position)
         else:
             raise ValueError("Argument must be array-like (list or ndarray)")
+
+    def _average_discrete_velocity(self, distances, timestamps):
+        if len(distances) != len(timestamps):
+            raise ValueError("Every distance entry must have a corresponding timestamp")
+        velocity_sum = 0
+        for idx in range(0, len(distances)):
+            if idx != len(distances) - 1:
+                update = self._discrete_velocity(distances[idx], distances[idx + 1], timestamps[idx], timestamps[idx + 1])
+                if abs(update) < 2.0:
+                    velocity_sum += update
+        return velocity_sum / int(len(distances) / 2)
+
+    def _discrete_velocity(self, dist_before, dist_after, timestamp_before, timestamp_after):
+        return (dist_after - dist_before) / (timestamp_before - timestamp_after)

cc90b5986f255b579601a1f3f7ab14bfb540dde7
Carl Mueller <cmuell89@gmail.com>
2019-06-10 12:04:57 -0600

    PEP8 formatting.

--- lfd_experiments/prototyping/vbgmm_segmentation/V2.py
+++ lfd_experiments/prototyping/vbgmm_segmentation/V2.py
@@ -17,16 +17,18 @@ from scipy import linalg
 import matplotlib
 import matplotlib.pyplot as plt
 
-from mpl_toolkits.mplot3d import axes3d, Axes3D #<-- Note the capitalization!
+from mpl_toolkits.mplot3d import axes3d, Axes3D  # <-- Note the capitalization!
 
 color_iter = itertools.cycle(['navy', 'black', 'cornflowerblue', 'r',
                               'darkorange', 'g', 'brown'])
 
+
 def load_json_file(path):
-        # Load a single file with one function call
-        with open(path, 'r') as f:
-            datastore = json.load(f)
-            return datastore
+    # Load a single file with one function call
+    with open(path, 'r') as f:
+        datastore = json.load(f)
+        return datastore
+
 
 def load_json_files(path, count=None):
     # Load multiple files with one function call
@@ -44,6 +46,7 @@ def load_json_files(path, count=None):
                 raise  # Propagate other kinds of IOError.
     return entries
 
+
 def vectorize_demonstrations(demonstrations):
     # Retrieve position coordinates for multiple demonstrations
     vectorized_demonstrations = []
@@ -56,6 +59,7 @@ def vectorize_demonstrations(demonstrations):
         vectorized_demonstrations.append(vectorized_demo)
     return vectorized_demonstrations
 
+
 def vectorize_demonstration(demonstration):
     # Retrieve position coordinates for a single demonstration
     vectorized_demonstrations = []
@@ -67,7 +71,8 @@ def vectorize_demonstration(demonstration):
     vectorized_demonstrations.append(vectorized_demo)
     return vectorized_demonstrations
 
-def plot_results(X, Y_, means, covariances, fig = plt.figure()):
+
+def plot_results(X, Y_, means, covariances, fig=plt.figure()):
     ax = fig.add_subplot(111, projection='3d')
     for i, (mean, covar, color) in enumerate(zip(
             means, covariances, color_iter)):
@@ -77,7 +82,8 @@ def plot_results(X, Y_, means, covariances, fig = plt.figure()):
         # components.
         if not np.any(Y_ == i):
             continue
-        ax.scatter(X[Y_ == i, 0], X[Y_ == i, 1], X[Y_ ==i, 2], s=.8, color=color)
+        ax.scatter(X[Y_ == i, 0], X[Y_ == i, 1],
+                   X[Y_ == i, 2], s=.8, color=color)
 
         # find the rotation matrix and radii of the axes
         U, s, rotation = linalg.svd(covar)
@@ -91,11 +97,14 @@ def plot_results(X, Y_, means, covariances, fig = plt.figure()):
         z = radii[2] * np.outer(np.ones_like(u), np.cos(v))
         for i in range(len(x)):
             for j in range(len(x)):
-                [x[i, j], y[i, j], z[i, j]] = np.dot([x[i, j], y[i, j], z[i, j]], rotation) + mean
+                [x[i, j], y[i, j], z[i, j]] = np.dot(
+                    [x[i, j], y[i, j], z[i, j]], rotation) + mean
 
         # plot
-        ax.plot_wireframe(x, y, z, rstride=4, cstride=4, color=color, alpha=0.2)
+        ax.plot_wireframe(x, y, z, rstride=4, cstride=4,
+                          color=color, alpha=0.2)
     return ax
+
 
 class VBGMMSegmentation():
     def __init__(self, alldemos):
@@ -109,10 +118,11 @@ class VBGMMSegmentation():
         n_samples_tot = len(X)
         print(n_samples_tot)
         if n_samples_tot < 10:
-           self.vbgmm = VariationalGMM(n_components=n_samples_tot).fit(X)
+            self.vbgmm = VariationalGMM(n_components=n_samples_tot).fit(X)
         else:
-           self.vbgmm = VariationalGMM(n_components=10).fit(X)
-        plot_results(X, self.vbgmm.predict(X), self.vbgmm.means_, self.vbgmm.covariances_)
+            self.vbgmm = VariationalGMM(n_components=10).fit(X)
+        plot_results(X, self.vbgmm.predict(
+            X), self.vbgmm.means_, self.vbgmm.covariances_)
         plt.show()
 
     def segment(self, data):
@@ -120,7 +130,6 @@ class VBGMMSegmentation():
         demo = vectorize_demonstration(data)
         X = np.array([e for sl in demo for e in sl])
         n_samples = len(X)
-        print(X)
         prediction = self.vbgmm.predict(X)
 
         # Find start and end indices for each observation
@@ -130,32 +139,35 @@ class VBGMMSegmentation():
             if i == 0:
                 currentnum = prediction[i]
                 startindices.append(i)
-            elif (prediction[i] != currentnum) & (i == len(prediction)-1):
-                endindices.append(i-1)
+            elif (prediction[i] != currentnum) & (i == len(prediction) - 1):
+                endindices.append(i - 1)
                 startindices.append(i)
                 endindices.append(i)
                 currentnum = prediction[i]
             elif (prediction[i] != currentnum):
-                endindices.append(i-1)
+                endindices.append(i - 1)
                 startindices.append(i)
                 currentnum = prediction[i]
-            elif i == len(prediction)-1:
+            elif i == len(prediction) - 1:
                 endindices.append(i)
 
         # Use start and end indices to create/splice segments
         segments = []
         for index in range(len(startindices)):
             if startindices[index] == endindices[index]:
-                segments.append(data[startindices[index]:endindices[index]+1])
+                segments.append(
+                    data[startindices[index]:endindices[index] + 1])
             else:
                 segments.append(data[startindices[index]:endindices[index]])
 
         # Return
-        return segments        
+        return segments
+
 
 class Segment():
-    def __init__(self,segments):
+    def __init__(self, segments):
         self.segments = segments
+
 
 class DemonstrationSegmenter():
     def __init__(self, segmenter):
@@ -170,21 +182,21 @@ class DemonstrationSegmenter():
         # {
         #   1: [segment1for1, segment2for1, segment3for1],
         #   2: [segment1for2, segment2for2, segment3for2]
-        # }    		
-    	return allsegments
+        # }
+        return allsegments
 
     def segment_classification(self, allsegments):
         temp = {}
         for demonum in range(len(allsegments)):
             for segnum in range(len(allsegments[demonum])):
                 if segnum not in temp:
-                    temp[segnum] = {demonum:allsegments[demonum][segnum]}
+                    temp[segnum] = {demonum: allsegments[demonum][segnum]}
                 else:
                     temp[segnum][demonum] = allsegments[demonum][segnum]
 
         segobjects = []
         for item in temp:
-            segobjects.append(Segment({key:temp[key] for key in [item]}))
+            segobjects.append(Segment({key: temp[key] for key in [item]}))
         # SegmentationObject1.segments -> {
         #     1: segment1for1,
         #     2: segment1for2
@@ -194,42 +206,50 @@ class DemonstrationSegmenter():
     def plot_segments(self, allsegments, segobjects, vbgmm):
         # Plot allsegments
         for demoind in range(len(allsegments)):
-            for segind in range(len(allsegments[demoind])):   
+            for segind in range(len(allsegments[demoind])):
                 segment = allsegments[demoind][segind]
                 if len(segment) > 1:
                     demo = vectorize_demonstration(segment)
                     X = np.array([e for sl in demo for e in sl])
                     if segind == 0:
-                        fig = plot_results(X, vbgmm.predict(X), vbgmm.means_, vbgmm.covariances_)
+                        fig = plot_results(X, vbgmm.predict(
+                            X), vbgmm.means_, vbgmm.covariances_)
                     else:
-                        plot_results(X, vbgmm.predict(X), vbgmm.means_, vbgmm.covariances_,fig)
+                        plot_results(X, vbgmm.predict(
+                            X), vbgmm.means_, vbgmm.covariances_, fig)
         plt.show()
         # {
         #   1: [segment1for1, segment2for1, segment3for1],
         #   2: [segment1for2, segment2for2, segment3for2]
-        # }         
+        # }
+
 
 def main():
     # Build model
-    dir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test_data')
+    dir_path = os.path.join(os.path.dirname(
+        os.path.realpath(__file__)), 'test_data')
     alldemos = load_json_files(os.path.join(dir_path, '*.json'), count=5)
     vbgmm_segmenter = VBGMMSegmentation(alldemos)
 
     # Deploy model to segment each demonstration
     demo_segmenter = DemonstrationSegmenter(vbgmm_segmenter)
-    dir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test_data')
-    jsonfiles = [f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))]
+    dir_path = os.path.join(os.path.dirname(
+        os.path.realpath(__file__)), 'test_data')
+    jsonfiles = [f for f in os.listdir(
+        dir_path) if os.path.isfile(os.path.join(dir_path, f))]
     demonstrations = []
     for file in jsonfiles:
-        data = load_json_file(os.path.join(dir_path,file))
+        data = load_json_file(os.path.join(dir_path, file))
         demonstrations.append(data)
     allsegments = demo_segmenter.segment_demonstrations(demonstrations)
     segobjects = demo_segmenter.segment_classification(allsegments)
 
     # Plot segments
-    demo_segmenter.plot_segments(allsegments,segobjects)
-    
+    demo_segmenter.plot_segments(
+        allsegments, segobjects, vbgmm_segmenter.vbgmm)
+
     print("ending")
+
 
 if __name__ == '__main__':
     main()

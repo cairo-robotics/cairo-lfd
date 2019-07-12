112a1f5d3cf90026c4e6ef686ff23c3ba38c25cd
Carl Mueller <cmuell89@gmail.com>
2019-07-12 14:51:43 -0600

    Metaconstraints
    Metaconstraints will be used to house heuristics, the constraint class, and keyframe to component assignment. Each component iwll be mapped to a meta constraint with the concrete instantiations of parameterized constraints. These MetaConstraints will then be assigned to the keyframes.

---
+++ cairo_lfd/src/cairo_lfd/constraints/assignment.py
@@ -0,0 +1,20 @@
+class MetaConstraintAssignment():
+
+    def __init__(self, segment_model, constraint_class):
+        self.segment_model = segment_model
+        self.signature = signature
+        self.constraint_class = constraint_class
+
+
+    def predict_component(self, vectors):
+        pass
+
+
+class MetaConstraint():
+
+    def __init__(self, constraint_class, parameters):
+        self.constraint_class = constraint_class
+        self.parameters = parameters
+
+    def generate_constraint_
+

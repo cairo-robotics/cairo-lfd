c424459d9d71783e444858b2e3ba389f6e3368e1
Carl Mueller <cmuell89@gmail.com>
2019-07-22 10:39:14 -0600

    Prototyping separation of constraint triggers.

---
+++ lfd_experiments/prototyping/constraint_triggers_test/constraint_triggers_test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python2
+
+import rospy
+import argparse
+import intera_interface
+from intera_interface import CHECK_VERSION
+from intera_motion_interface import InteractionOptions, InteractionPublisher
+from cairo_lfd.core.record import SawyerRecorder
+from cairo_lfd.core.environment import Environment, import_configuration
+from cairo_lfd.core.items import ItemFactory
+from cairo_lfd.constraints.concept_constraints import ConstraintFactory
+from cairo_lfd.constraints.triggers import TriggerFactory
+from cairo_lfd.modeling.analysis import ConstraintAnalyzer
+from cairo_lfd.data.io import DataExporter
+
+
+def main():
+    """
+    Demonstration Recorder
+
+    Record a series of demonstrations.
+    """
+
+    arg_fmt = argparse.RawDescriptionHelpFormatter
+    parser = argparse.ArgumentParser(formatter_class=arg_fmt,
+                                     description=main.__doc__)
+    required = parser.add_argument_group('required arguments')
+
+    required.add_argument(
+        '-c', '--config', dest='config', required=True,
+        help='the file path of the demonstration '
+    )
+    args = parser.parse_args(rospy.myargv()[1:])
+
+    config_filepath = args.config
+    configs = import_configuration(config_filepath)
+
+    triggers = TriggerFactory(configs).generate_triggers()
+
+    for trigger in triggers:
+        print(type(trigger))
+
+if __name__ == '__main__':
+    main()

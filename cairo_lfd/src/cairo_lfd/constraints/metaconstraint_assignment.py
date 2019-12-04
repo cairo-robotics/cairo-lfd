067e46ddf04bb5b164c7964119cfa93f655e18fd
Carl Mueller <cmuell89@gmail.com>
2019-12-04 12:46:12 -0700

    Metaconstraint assignment

--- cairo_lfd/src/cairo_lfd/constraints/metaconstraint_assignment.py
+++ cairo_lfd/src/cairo_lfd/constraints/metaconstraint_assignment.py
@@ -1,35 +1,90 @@
 from collections import Counter
-
+from functools import partial
+from copy import copy
+import pudb
 import numpy as np
 
-from cairo_lfd.constraints.metaconstraints import HeightMetaconstraint, UprightMetaconstraint, OverUnderMetaconstraint, Perimeter2DMetaconstraint
-from cairo_lfd.data.vectorization import boolean_within_SOI, boolean_within_perimeter, xy_radial_distance, vectorize_robot_position, vectorize_robot_orientation
+from cairo_lfd.constraints.metaconstraints import HeightMetaconstraint, OrientationMetaconstraint, OverUnderMetaconstraint, Perimeter2DMetaconstraint
+from cairo_lfd.data.vectorization import vectorize_demonstration, boolean_within_SOI, boolean_within_perimeter, xy_radial_distance, vectorize_robot_position, vectorize_robot_orientation, xy_radial_distance
+
+from cairo_lfd.constraints.segmentation import BayesianGMMSegmentModel
+from cairo_lfd.constraints.heuristics import HeightHeuristicModel, OrientationHeuristicModel, OverUnderHeuristicModel, PerimeterHeuristicModel
 
 
 class MetaconstraintAssigner():
 
-    def __init__(self, environment, graph, metaconstraint_builders):
-        self.env = environment
+    def __init__(self, graph, metaconstraint_builders):
         self.graph = graph
         self.builders = metaconstraint_builders
 
-    def assign_metaconstraints(self,):
+    def assign_metaconstraints(self):
         for node in self.graph.get_keyframe_sequence():
             for builder in self.builders:
-                self.graph.nodes[node]['metaconstraints'].append(builder.build_metaconstraint(self.graph.nodes[node], self.env))
+                name, metaconstraints = builder.build_metaconstraint(self.graph.nodes[node])
+                if metaconstraints is not None:
+                    self.graph.nodes[node]['metaconstraints'][name] = metaconstraints
+
+
+class MetaconstraintBuilderFactory():
+
+    def __init__(self, configs):
+        self.configs = configs
+        self.builder_configs = {
+            "OrientationMetaconstraintBuilder": {
+                "vectorizor": lambda demos: np.vstack(np.array(map(partial(vectorize_demonstration, vectorizors=[vectorize_robot_orientation]), demos))),
+                "segmentation_model": BayesianGMMSegmentModel,
+                "heuristic_model": OrientationHeuristicModel,
+                "metaconstraint_builder": OrientationMetaconstraintBuilder
+            },
+            "HeightMetaconstraintBuilder": {
+                "vectorizor": lambda demos: np.vstack(np.array(map(partial(vectorize_demonstration, vectorizors=[vectorize_robot_position]), demos))),
+                "segmentation_model": BayesianGMMSegmentModel,
+                "heuristic_model": HeightHeuristicModel,
+                "metaconstraint_builder": HeightMetaconstraintBuilder
+            },
+            "OverUnderMetaconstraintBuilder": {
+                "vectorizor": None,
+                "segmentation_model": None,
+                "heuristic_model": OverUnderHeuristicModel,
+                "metaconstraint_builder": OverUnderMetaconstraintBuilder
+            },
+            "Perimeter2DMetaconstraintBuilder": {
+                "vectorizor": None,
+                "segmentation_model": None,
+                "heuristic_model": PerimeterHeuristicModel,
+                "metaconstraint_builder": Perimeter2DMetaconstraintBuilder
+            }
+        }
+
+    def generate_metaconstraint_builders(self, demonstrations):
+        builders = []
+        for builder in self.configs["metaconstraint_builders"]:
+            builder_config = self.builder_configs[builder["class"]]
+            static_params = builder['static_parameters']
+            if builder_config["segmentation_model"] is not None and builder_config["vectorizor"] is not None:
+                vectorizor = builder_config["vectorizor"]
+                X = vectorizor(demonstrations)
+                segmentation_model = builder_config["segmentation_model"](X, **builder['segmentation']['init_args'])
+                heuristic_model = builder_config["heuristic_model"](segmentation_model)
+            else:
+                heuristic_model = builder_config["heuristic_model"]()
+            heuristic_model.fit()
+            builders.append(builder_config["metaconstraint_builder"](builder['name'], heuristic_model, static_params))
+        return builders
 
 
 class HeightMetaconstraintBuilder():
 
-    def __init__(self, heuristic_model, static_parameters):
+    def __init__(self, name, heuristic_model, static_parameters):
+        self.name = name
         self.heuristic_model = heuristic_model
         self.static_parameters = static_parameters
 
-    def build_metaconstraint(self, keyframe_node, environment=None):
-        metaconstraint = HeightMetaconstraint(self.static_parameters)
+    def build_metaconstraint(self, keyframe_node):
+        metaconstraint = HeightMetaconstraint(self.name, self.static_parameters)
         heuristic_parameters = self._get_heuristic_parameters(keyframe_node)
         metaconstraint.parameterize_constraints(heuristic_parameters)
-        return metaconstraint
+        return self.name, metaconstraint
 
     def _get_heuristic_parameters(self, keyframe_node):
         vectors = np.array([vectorize_robot_position(obs) for obs in keyframe_node['observations']])
@@ -38,15 +93,16 @@ class HeightMetaconstraintBuilder():
 
 class OrientationMetaconstraintBuilder():
 
-    def __init__(self, heuristic_model, static_parameters):
+    def __init__(self, name, heuristic_model, static_parameters):
+        self.name = name
         self.heuristic_model = heuristic_model
         self.static_parameters = static_parameters
 
-    def build_metaconstraint(self, keyframe_node, environment=None):
-        metaconstraint = UprightMetaconstraint(self.static_parameters)
+    def build_metaconstraint(self, keyframe_node):
+        metaconstraint = OrientationMetaconstraint(self.name, self.static_parameters)
         heuristic_parameters = self._get_heuristic_parameters(keyframe_node)
         metaconstraint.parameterize_constraints(*heuristic_parameters)
-        return metaconstraint
+        return self.name, metaconstraint
 
     def _get_heuristic_parameters(self, keyframe_node):
         vectors = np.array([vectorize_robot_orientation(obs) for obs in keyframe_node['observations']])
@@ -55,51 +111,51 @@ class OrientationMetaconstraintBuilder():
 
 class OverUnderMetaconstraintBuilder():
 
-    def __init__(self, heuristic_model, static_parameters):
+    def __init__(self, name, heuristic_model, static_parameters):
+        self.name = name
         self.heuristic_model = heuristic_model
         self.static_parameters = static_parameters
 
-    def build_metaconstraint(self, keyframe_node, environment=None):
+    def build_metaconstraint(self, keyframe_node):
         if self._validate_keyframe(keyframe_node):
-            metaconstraint = OverUnderMetaconstraint(static_parameters)
+            metaconstraint = OverUnderMetaconstraint(self.name, self.static_parameters)
             heuristic_parameters = self._get_heuristic_parameters(keyframe_node)
             metaconstraint.parameterize_constraints(heuristic_parameters)
-            return metaconstraint
-        return None
+            return self.name, metaconstraint
+        return self.name, None
 
     def _validate_keyframe(self, keyframe_node):
         above_item_id = self.static_parameters["above_item_id"]
         below_item_id = self.static_parameters["below_item_id"]
-        boolean_vectors = [boolean_within_perimeter(obs, above_item_id, below_item_id) for obs in keyframe_node['observations']]
-        predictions_counter = Counter(predictions)
-        return predictions_counter.most_common(1)[0][0]
+        boolean_vectors = [boolean_within_SOI(obs, above_item_id, below_item_id) for obs in keyframe_node['observations']]
+        counter = Counter(boolean_vectors)
+        return counter.most_common(1)[0][0]
 
     def _get_heuristic_parameters(self, keyframe_node):
-        return self.heuristic_model.get_parameters(np.array([xy_radial_distance(obs) for obs in keyframe_node['observations']]))
+        return self.heuristic_model.get_parameters(np.array([xy_radial_distance(obs, self.static_parameters["above_item_id"], self.static_parameters["below_item_id"]) for obs in keyframe_node['observations']]))
 
 
 class Perimeter2DMetaconstraintBuilder():
 
-    def __init__(self, heuristic_model, static_parameters):
+    def __init__(self, name, heuristic_model, static_parameters):
+        self.name = name
         self.heuristic_model = heuristic_model
-        self.metaconstraint_class = Perimeter2DMetaconstraint(static_parameters)
         self.static_parameters = static_parameters
 
-    def build_metaconstraint(self, keyframe_node, environment=None):
+    def build_metaconstraint(self, keyframe_node):
+        metaconstraint = Perimeter2DMetaconstraint(self.name, self.static_parameters)
         if self._validate_keyframe(keyframe_node):
-            heuristic_parameters = self._get_heuristic_parameters(keyframe_node)
-            self.metaconstraint.parameterize_constraints(heuristic_parameters)
-            return self.metaconstraint
-        return None
+            heuristic_parameters = self._get_heuristic_parameters()
+            metaconstraint.parameterize_constraints(heuristic_parameters)
+            return self.name, metaconstraint
+        return self.name, None
 
     def _validate_keyframe(self, keyframe_node):
-        perimeter_item_id = static_parameters["perimeter_item_id"]
-        traversing_item_id = static_parameters["traversing_item_id"]
+        perimeter_item_id = self.static_parameters["perimeter_item_id"]
+        traversing_item_id = self.static_parameters["traversing_item_id"]
         boolean_vectors = [boolean_within_perimeter(obs, perimeter_item_id, traversing_item_id) for obs in keyframe_node['observations']]
-        predictions_counter = Counter(predictions)
-        return predictions_counter.most_common(1)[0][0]
+        counter = Counter(boolean_vectors)
+        return counter.most_common(1)[0][0]
 
-    def _get_heuristic_parameters(self, keyframe_node, environment):
-        curr_item_state = self.environment.get_item_state_by_id(static_parameters["perimeter_item_id"])
-        return self.heuristic_model.get_parameters(curr_item_state)
- 
+    def _get_heuristic_parameters(self,):
+        return self.heuristic_model.get_parameters()
--- cairo_lfd/src/cairo_lfd/constraints/metaconstraints.py
+++ cairo_lfd/src/cairo_lfd/constraints/metaconstraints.py
@@ -1,59 +1,65 @@
 from collections import Counter
 
-from cairo_lfd.constraints.concept_constraints import HeightConstraint, UprightConstraint, Perimeter2DConstraint, OverUnderConstraint
+from cairo_lfd.constraints.concept_constraints import HeightConstraint, OrientationConstraint, Perimeter2DConstraint, OverUnderConstraint
 
 
 class HeightMetaconstraint():
 
-    def __init__(self, static_parameters):
+    def __init__(self, name, static_parameters):
+        self.name = name
         self.static_params = static_parameters
         self.constraint_class = HeightConstraint
         self.valid_constraints = []
 
     def parameterize_constraints(self, discrete_heights):
         constraints = []
+        # More constrained is higher height so we sort in descending order.
+        discrete_heights = sorted(discrete_heights, reverse=True)
         for idx, height in enumerate(discrete_heights):
-            constraints.append(HeightConstraint(constraint_id=idx,
+            constraints.append(HeightConstraint(constraint_id=(self.name, idx),
                                                 threshold_distance=height, **self.static_params))
         self.constraints = constraints
 
 
-class UprightMetaconstraint():
+class OrientationMetaconstraint():
 
-    def __init__(self, static_parameters):
+    def __init__(self, name, static_parameters):
+        self.name = name
         self.static_params = static_parameters
-        self.constraint_class = UprightConstraint
+        self.constraint_class = OrientationConstraint
 
     def parameterize_constraints(self, avg_orientation, angles_of_deviation):
         constraints = []
+        angles_of_deviation = sorted(angles_of_deviation)
         for idx, angle in enumerate(angles_of_deviation):
-            constraints.append(UprightConstraint(constraint_id=idx,
-                                                reference_orientation=avg_orientation, threshold_angle=angle, **self.static_params))
+            constraints.append(OrientationConstraint(constraint_id=(self.name, idx),
+                                                     reference_orientation=avg_orientation, threshold_angle=angle, **self.static_params))
         self.constraints = constraints
 
 
 class Perimeter2DMetaconstraint():
 
-    def __init__(self, static_parameters):
-        self.heuristic_params = heuristic_parameters
+    def __init__(self, name, static_parameters):
+        self.name = name
         self.static_params = static_parameters
         self.constraint_class = Perimeter2DConstraint
-        self.perimeter_heuristic = perimeter_heuristic
 
     def parameterize_constraints(self, heuristic_parameters=None):
-        pass
+        # There is nothing to parameterize. The function parameters exist to support interfaceing to Metaconstraint Builders.
+        self.constraints = [Perimeter2DConstraint(constraint_id=(self.name, 0), **self.static_params)]
 
 
 class OverUnderMetaconstraint():
 
-    def __init__(self, static_parameters):
+    def __init__(self, name, static_parameters):
+        self.name = name
         self.static_params = static_parameters
         self.constraint_class = OverUnderConstraint
-        self.over_under_heuristic = over_under_heuristic
 
     def parameterize_constraints(self, discrete_radii):
         constraints = []
-        for idx, radii in enumerate(discrete_heights):
-            constraints.append(OverUnderConstraint(constraint_id=idx,
-                                                threshold_distance=height, **self.static_params))
+        discrete_radii = sorted(discrete_radii)
+        for idx, threshold_radius in enumerate(discrete_radii):
+            constraints.append(OverUnderConstraint(constraint_id=(self.name, idx),
+                                                   threshold_distance=threshold_radius, **self.static_params))
         self.constraints = constraints

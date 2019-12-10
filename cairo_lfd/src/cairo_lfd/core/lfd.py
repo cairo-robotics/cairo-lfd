ac2bd2f15f2bdd3864d8740676a132b322caae22
Carl Mueller <cmuell89@gmail.com>
2019-12-09 19:40:57 -0700

    Preliminary publish message for applied constraints during skill execuition.

--- cairo_lfd/src/cairo_lfd/core/lfd.py
+++ cairo_lfd/src/cairo_lfd/core/lfd.py
@@ -18,6 +18,8 @@ from cairo_lfd.modeling.models import KDEModel
 from cairo_lfd.modeling.sampling import KeyframeSampler, ModelScoreSampleRanker, ConfigurationSpaceSampleRanker, MetaconstraintSampler
 from cairo_lfd.modeling.analysis import KeyframeGraphAnalyzer, ConstraintAnalyzer
 
+from cairo_lfd_msgs.msg import KeyframeConstraints
+
 
 class ACC_LFD():
 
@@ -270,3 +272,36 @@ class CC_LFD():
             joint_config_array.append(joints)
 
         self.moveit_interface.move_to_joint_targets(joint_config_array)
+
+    def perform_skill(self):
+        """ Create a sequence of keyframe way points and execute motion plans to reconstruct skill """
+
+        # Create publisher for node information
+        time_pub = rospy.Publisher('/lfd/node_time', NodeTime, queue_size=10)
+        constraint_pub = rospy.Publisher('/lfd/applied_constraints', KeyframeConstraints)
+
+        for i in range(len(self.graph.get_keyframe_sequence()) - 1):
+            rospy.loginfo("LFD: Moving to a new point...")
+
+            # Grab nodes, samples, and joints
+            cur_node = self.graph.get_keyframe_sequence()[i]
+            next_node = self.graph.get_keyframe_sequence()[i + 1]
+            cur_sample = self.graph.nodes[cur_node]["samples"][0]
+            next_sample = self.graph.nodes[next_node]["samples"][0]
+            cur_joints = cur_sample.get_joint_angle()
+            next_joints = next_sample.get_joint_angle()
+
+            # Build and publish node data
+            time_msg = NodeTime()
+            time_msg.cur_node = int(cur_node)
+            time_msg.next_node = int(next_node)
+            time_msg.timestamp = rospy.Time.now()
+            time_pub.publish(time_msg)
+
+            constraints = self.graph.nodes[cur_node]["applied_constraints"]
+            constraints_msg = KeyframeConstraints()
+            constraints_msg.constraints = constraints
+            constraint_pub.publish(constraints_msg)
+
+            # Execute movement using MoveIt!
+            self.moveit_interface.move_to_joint_targets([cur_joints, next_joints])

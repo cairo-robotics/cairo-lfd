3ba1fda3dc3cb7e35c9e1a8d97752b7030c3ac33
Carl Mueller <cmuell89@gmail.com>
2020-02-17 13:27:35 -0700

    PEP8!!

--- lfd_experiments/scripts/ar_representation_publisher.py
+++ lfd_experiments/scripts/ar_representation_publisher.py
@@ -8,16 +8,18 @@ from std_msgs.msg import String
 
 
 def main():
-	pub = rospy.Publisher("/cairo_lfd/lfd_representation", String, queue_size=10)
-	time.sleep(.5)
-	json_str = None
-	with open("./cubby_representation.txt", "r") as f:
-		json_str = json.loads(f.read())
+    pub = rospy.Publisher("/cairo_lfd/lfd_representation",
+                          String, queue_size=10)
+    time.sleep(.5)
+    json_str = None
+    with open("./cubby_representation.txt", "r") as f:
+        json_str = json.loads(f.read())
 
-	msg = String()
-	msg.data = json.dumps(json_str)
-	pub.publish(msg)
+    msg = String()
+    msg.data = json.dumps(json_str)
+    pub.publish(msg)
+
 
 if __name__ == '__main__':
-	rospy.init_node("my_publisher_node")
-	main()
+    rospy.init_node("my_publisher_node")
+    main()

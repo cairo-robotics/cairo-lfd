1055e8a0927194f73b9086f5c611b1895150a7b8
Carl Mueller <cmuell89@gmail.com>
2020-03-04 15:29:38 -0700

    Configuration error now in io.py

--- cairo_lfd/src/cairo_lfd/core/environment.py
+++ cairo_lfd/src/cairo_lfd/core/environment.py
@@ -467,15 +467,6 @@ class Observation(object):
             return None
 
 
-class ConfigurationError(Exception):
-
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self): 
-        return(repr(self.value))
-
-
 class EnvironmentError(Exception):
 
     def __init__(self, value):

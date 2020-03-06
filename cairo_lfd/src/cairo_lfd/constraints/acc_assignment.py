f198bae7580ff3a9ed416a4abe880e1a7edda5a0
Carl Mueller <cmuell89@gmail.com>
2020-03-05 17:12:10 -0700

    acc_assignment.py documentation

--- cairo_lfd/src/cairo_lfd/constraints/acc_assignment.py
+++ cairo_lfd/src/cairo_lfd/constraints/acc_assignment.py
@@ -1,3 +1,5 @@
+"""Summary
+"""
 from collections import Counter
 from functools import partial
 from copy import copy
@@ -13,6 +15,16 @@ from cairo_lfd.constraints.heuristics import HeightHeuristicModel, OrientationHe
 
 
 def assign_autoconstraints(self, graph, autoconstraint_builders):
+    """
+    Assigns Autoconstraints generated from each of the AutoconstraintBuilders to each node in a keyframe graph.
+
+    Parameters
+    ----------
+    graph : cairo_lfd.graphing.KeyframeGraph
+        The Keyframe Graph to which Autoconstraints will be added to.
+    autoconstraint_builders : list
+        A list of AutonconstraintBuilder objects that each build a parameterized set of Autoconstraints.
+    """
     for node in self.graph.get_keyframe_sequence():
         for builder in self.builders:
             name, autoconstraints = builder.build_autoconstraint(
@@ -22,8 +34,23 @@ def assign_autoconstraints(self, graph, autoconstraint_builders):
 
 
 class AutoconstraintFactory():
+    """
+    A factory-patterned class that creates AutoconstraintBuilder Objects.
 
+    Attributes
+    ----------
+    builder_configs : dict
+        Static set of available AutoconstraintBuilders and their required dependencies.
+    configs : dict
+        Autoconstraint according to the config.json file imported for a study.
+    """
     def __init__(self, configs):
+        """
+        Parameters
+        ----------
+        configs : dict
+            Autoconstraint according to the config.json file imported for a study.
+        """
         self.configs = configs
         self.builder_configs = {
             "OrientationAutoconstraintBuilder": {
@@ -53,6 +80,19 @@ class AutoconstraintFactory():
         }
 
     def generate_autoconstraint_builders(self, demonstrations):
+        """
+        Generates AutoconstraintBuilders that will ultimately build and learn Autoconstraints on a set of demonstrations.
+
+        Parameters
+        ----------
+        demonstrations : list
+            List of cairo_lfd.environment.Demonstration objects. 
+
+        Returns
+        -------
+        : list
+            List of AutoconstraintBuilders
+        """
         builders = []
         for builder in self.configs["autoconstraint_builders"]:
             builder_config = self.builder_configs[builder["class"]]
@@ -75,14 +115,54 @@ class AutoconstraintFactory():
 
 
 class PlanarAutoconstraintBuilder():
+    """
+    Builds PlanarAutoconstraints given the provided parameters and heuristic model.
 
+    Attributes
+    ----------
+    heuristic_model : cairo_lfd.constraints.heuristics.HeightHeuristicModel
+        A heuristic model that guides how to parameterize the PlanarAutoconstraints.
+    name : str
+        Name to apply to the Autoconstraint
+    static_parameters : dict
+        Static parameters that are predetermined by the user in a config.json file.
+    validation_params : dict
+        Parameters used to determine if an Autoconstraint is applicable (may not be applicable for a autoconstraint).
+    """
     def __init__(self, name, heuristic_model, validation_params, static_parameters):
+        """
+        Parameters
+        ----------
+        name : str
+            Name to apply to the Autoconstraint
+        heuristic_model : cairo_lfd.constraints.heuristics.HeightHeuristicModel
+            A heuristic model that guides how to parameterize the PlanarAutoconstraints.
+        validation_params : dict
+            Parameters used to determine if an Autoconstraint is applicable (may not be applicable for a autoconstraint).
+        static_parameters : dict
+            Static parameters that are predetermined by the user in a config.json file.
+        """
         self.name = name
         self.heuristic_model = heuristic_model
         self.validation_params = validation_params
         self.static_parameters = static_parameters
 
     def build_autoconstraint(self, keyframe_node):
+        """
+        Builds the PlanarAutoconstraint object.
+
+        Parameters
+        ----------
+        keyframe_node : dict
+            KeyframeGraph node object (dict)
+
+        Returns
+        -------
+        self.name : str
+            Name of the Autoconstraint
+        autoconstraint : cairo_lfd.autoconstraints.PlanarAutoconstraint
+            The parameterized Autoconstraint.
+        """
         self._assign_constraint_transitions(keyframe_node)
         autoconstraint = PlanarAutoconstraint(
             self.name, self.static_parameters)
@@ -91,11 +171,33 @@ class PlanarAutoconstraintBuilder():
         return self.name, autoconstraint
 
     def _get_heuristic_parameters(self, keyframe_node):
+        """
+        Gets the constraint parameters from the learned heuristic model dependent on the passed keyframe_node's observation data.
+
+        Parameters
+        ----------
+        keyframe_node : dict
+            KeyframeGraph node object (dict)
+
+        Returns
+        -------
+        : list/dict
+            The generated parameters from the heuristic model.
+        """
         vectors = np.array([vectorize_robot_position(obs)
                             for obs in keyframe_node['observations']])
         return self.heuristic_model.get_parameters(vectors)
 
     def _assign_constraint_transitions(self, keyframe_node):
+        """
+        Assigns a unique auto constraint transition string to the keyframe_nodes autoconstraint_transitions field. This is used to determined the constraint transitions akin to the CC-LfD algorithm that generates constraint transition keyframe nodes that cannot be culled.
+
+        Parameters
+        ----------
+        keyframe_node : dict
+            KeyframeGraph node object (dict)
+
+        """
         vectors = np.array([vectorize_robot_position(obs)
                             for obs in keyframe_node['observations']])
         component = self.heuristic_model.assign_to_component(vectors)
@@ -103,14 +205,54 @@ class PlanarAutoconstraintBuilder():
 
 
 class OrientationAutoconstraintBuilder():
+    """
+    Builds OrientationAutoconstraints given the provided parameters and heuristic model.
 
+    Attributes
+    ----------
+    heuristic_model : cairo_lfd.constraints.heuristics.OrientationHeuristicModel
+        A heuristic model that guides how to parameterize the OrientationAutoconstraint.
+    name : str
+        Name to apply to the Autoconstraint
+    static_parameters : dict
+        Static parameters that are predetermined by the user in a config.json file.
+    validation_params : TYPE
+        Parameters used to determine if an Autoconstraint is applicable (may not be applicable for a autoconstraint).
+    """
     def __init__(self, name, heuristic_model, validation_params, static_parameters):
+        """
+        Parameters
+        ----------
+        name : str
+            Name to apply to the Autoconstraint
+        heuristic_model : cairo_lfd.constraints.heuristics.OrientationHeuristicModel
+            A heuristic model that guides how to parameterize the OrientationAutoconstraint.
+        validation_params : dict
+            Parameters used to determine if an Autoconstraint is applicable (may not be applicable for a autoconstraint).
+        static_parameters : dict
+            Static parameters that are predetermined by the user in a config.json file.
+        """
         self.name = name
         self.heuristic_model = heuristic_model
         self.validation_params = validation_params
         self.static_parameters = static_parameters
 
     def build_autoconstraint(self, keyframe_node):
+        """
+        Builds an OrientationAutoconstraint object.
+
+        Parameters
+        ----------
+        keyframe_node : dict
+            KeyframeGraph node object (dict)
+
+        Returns
+        -------
+        self.name : str
+            Name of the Autoconstraint
+        autoconstraint : cairo_lfd.autoconstraints.OrientationAutoconstraint
+            The parameterized Autoconstraint.
+        """
         self._assign_constraint_transitions(keyframe_node)
         autoconstraint = OrientationAutoconstraint(
             self.name, self.static_parameters)
@@ -119,11 +261,32 @@ class OrientationAutoconstraintBuilder():
         return self.name, autoconstraint
 
     def _get_heuristic_parameters(self, keyframe_node):
+        """
+        Gets the constraint parameters from the learned heuristic model dependent on the passed keyframe_node's observation data.
+
+        Parameters
+        ----------
+        keyframe_node : dict
+            KeyframeGraph node object (dict)
+
+        Returns
+        -------
+        : list/dict
+            The generated parameters from the heuristic model.
+        """
         vectors = np.array([vectorize_robot_orientation(obs)
                             for obs in keyframe_node['observations']])
         return self.heuristic_model.get_parameters(vectors)
 
     def _assign_constraint_transitions(self, keyframe_node):
+        """
+        Assigns a unique auto constraint transition string to the keyframe_nodes autoconstraint_transitions field. This is used to determined the constraint transitions akin to the CC-LfD algorithm that generates constraint transition keyframe nodes that cannot be culled.
+
+        Parameters
+        ----------
+        keyframe_node : dict
+            KeyframeGraph node object (dict)
+        """
         vectors = np.array([vectorize_robot_orientation(obs)
                             for obs in keyframe_node['observations']])
         component = self.heuristic_model.assign_to_component(vectors)
@@ -131,14 +294,54 @@ class OrientationAutoconstraintBuilder():
 
 
 class OverUnderAutoconstraintBuilder():
+    """
+    Builds OverUnderAutoconstraints given the provided parameters and heuristic model.
 
+    Attributes
+    ----------
+    heuristic_model : cairo_lfd.constraints.heuristics.OverUnderHeuristicModel
+        A heuristic model that guides how to parameterize the OverUnderAutoconstraint.
+    name : str
+        Name to apply to the Autoconstraint
+    static_parameters : dict
+        Static parameters that are predetermined by the user in a config.json file.
+    validation_params : TYPE
+        Parameters used to determine if an Autoconstraint is applicable (may not be applicable for a autoconstraint).
+    """
     def __init__(self, name, heuristic_model, validation_params, static_parameters):
+        """
+        Parameters
+        ----------
+        name : str
+            Name to apply to the Autoconstraint
+        heuristic_model : cairo_lfd.constraints.heuristics.OverUnderHeuristicModel
+            A heuristic model that guides how to parameterize the OverUnderAutoconstraint.
+        validation_params : dict
+            Parameters used to determine if an Autoconstraint is applicable (may not be applicable for a autoconstraint).
+        static_parameters : dict
+            Static parameters that are predetermined by the user in a config.json file.
+        """
         self.name = name
         self.heuristic_model = heuristic_model
         self.validation_params = validation_params
         self.static_parameters = static_parameters
 
     def build_autoconstraint(self, keyframe_node):
+        """
+        Builds an OverUnderAutoconstraint object.
+
+        Parameters
+        ----------
+        keyframe_node : dict
+            KeyframeGraph node object (dict)
+
+        Returns
+        -------
+        self.name : str
+            Name of the Autoconstraint
+        autoconstraint : cairo_lfd.autoconstraints.OverUnderAutoconstraint
+            The parameterized Autoconstraint.
+        """
         self._assign_constraint_transitions(keyframe_node)
         if self._validate_keyframe(keyframe_node):
             autoconstraint = OverUnderAutoconstraint(
@@ -150,6 +353,24 @@ class OverUnderAutoconstraintBuilder():
         return self.name, None
 
     def _validate_keyframe(self, keyframe_node):
+        """
+        Validate whether or not a keyframe_node is a viable candidate for the OverUnderAutoconstraint.
+
+        Parameters
+        ----------
+        keyframe_node : dict
+            KeyframeGraph node object (dict)
+
+        Returns
+        -------
+        : bool
+            Whether or not the keyframe node is a candidate for OverUnderAutoconstraint application.
+
+        Raises
+        ------
+        Exception
+            There must be a reference_pose if the static parameters do not contain the below item id.
+        """
         if self.static_parameters.get("below_item_id", None) is not None:
             above_item_id = self.static_parameters["above_item_id"]
             below_item_id = self.static_parameters["below_item_id"]
@@ -167,6 +388,19 @@ class OverUnderAutoconstraintBuilder():
             return counter.most_common(1)[0][0]
 
     def _get_heuristic_parameters(self, keyframe_node):
+        """
+        Gets the constraint parameters from the learned heuristic model dependent on the passed keyframe_node's observation data.
+
+        Parameters
+        ----------
+        keyframe_node : dict
+            KeyframeGraph node object (dict)
+
+        Returns
+        -------
+        : list/dict
+            The generated parameters from the heuristic model.
+        """
         if self.static_parameters.get("below_item_id", None) is not None: 
             return self.heuristic_model.get_parameters(np.array([xy_radial_distance(obs, self.static_parameters["above_item_id"], self.static_parameters["below_item_id"]) for obs in keyframe_node['observations']]))
         else:
@@ -174,19 +408,67 @@ class OverUnderAutoconstraintBuilder():
             return self.heuristic_model.get_parameters(np.array([xy_radial_distance_with_static(obs, self.static_parameters["above_item_id"], xy_static) for obs in keyframe_node['observations']]))
 
     def _assign_constraint_transitions(self, keyframe_node):
+        """
+        Assigns a unique auto constraint transition string to the keyframe_nodes autoconstraint_transitions field. This is used to determined the constraint transitions akin to the CC-LfD algorithm that generates constraint transition keyframe nodes that cannot be culled.
+
+        Parameters
+        ----------
+        keyframe_node : dict
+            KeyframeGraph node object (dict)
+        """
         validity = self._validate_keyframe(keyframe_node)
         keyframe_node["autoconstraint_transitions"].append(self.name + "_" + str(validity))
 
 
 class Perimeter2DAutoconstraintBuilder():
+    """
+    Builds Perimeter2DAutoconstraint given the provided parameters and heuristic model.
 
+    Attributes
+    ----------
+    heuristic_model : cairo_lfd.constraints.heuristics.PerimeterHeuristicModel
+        A heuristic model that guides how to parameterize the Perimeter2DAutoconstraint.
+    name : str
+        Name to apply to the Autoconstraint
+    static_parameters : dict
+        Static parameters that are predetermined by the user in a config.json file.
+    validation_params : TYPE
+        Parameters used to determine if an Autoconstraint is applicable (may not be applicable for a autoconstraint).
+    """
     def __init__(self, name, heuristic_model, validation_params, static_parameters):
+        """
+        Parameters
+        ----------
+        name : str
+            Name to apply to the Autoconstraint
+        heuristic_model : cairo_lfd.constraints.heuristics.PerimeterHeuristicModel
+            A heuristic model that guides how to parameterize the Perimeter2DAutoconstraint.
+        validation_params : dict
+            Parameters used to determine if an Autoconstraint is applicable (may not be applicable for a autoconstraint).
+        static_parameters : dict
+            Static parameters that are predetermined by the user in a config.json file.
+        """
         self.name = name
         self.heuristic_model = heuristic_model
         self.validation_params = validation_params
         self.static_parameters = static_parameters
 
     def build_autoconstraint(self, keyframe_node):
+        """
+        Builds an Perimeter2DAutoconstraint object.
+
+        Parameters
+        ----------
+        keyframe_node : dict
+            KeyframeGraph node object (dict)
+
+        Returns
+        -------
+        self.name : str
+            Name of the Autoconstraint
+        autoconstraint : cairo_lfd.autoconstraints.Perimeter2DAutoconstraint
+            The parameterized Autoconstraint.
+        """
         heuristic_parameters = Perimeter2DAutoconstraint(
             self.name, self.static_parameters)
         self._assign_constraint_transitions(keyframe_node)
@@ -197,6 +479,14 @@ class Perimeter2DAutoconstraintBuilder():
         return self.name, None
 
     def _validate_keyframe(self, keyframe_node):
+        """
+        The parameters are determined exclusively by the data, no node specificity is needed.
+
+        Returns
+        -------
+        : list/dict
+            The generated parameters from the heuristic model.
+        """
         perimeter_item_id = self.static_parameters["perimeter_item_id"]
         traversing_item_id = self.static_parameters["traversing_item_id"]
         boolean_vectors = [boolean_within_perimeter(
@@ -205,10 +495,24 @@ class Perimeter2DAutoconstraintBuilder():
         return counter.most_common(1)[0][0]
 
     def _get_heuristic_parameters(self,):
+        """
+        The parameters are determined exclusively by the data, no node specificity is needed.
+
+        Returns
+        -------
+        : list/dict
+            The generated parameters from the heuristic model.
+        """
         return self.heuristic_model.get_parameters()
 
     def _assign_constraint_transitions(self, keyframe_node):
+        """
+        Assigns a unique auto constraint transition string to the keyframe_nodes autoconstraint_transitions field. This is used to determined the constraint transitions akin to the CC-LfD algorithm that generates constraint transition keyframe nodes that cannot be culled.
+
+        Parameters
+        ----------
+        keyframe_node : dict
+            KeyframeGraph node object (dict)
+        """
         validity = self._validate_keyframe(keyframe_node)
         keyframe_node["autoconstraint_transitions"].append(self.name + "_" + str(validity))
-
-

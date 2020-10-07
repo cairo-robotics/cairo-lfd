cf3297eaeb962d000acf870a3c9f6b1afb300057
Carl Mueller <cmuell89@gmail.com>
2020-10-06 22:00:37 -0600

    Separating out creation of robots and passing explitly the configs for each factory
    Robots.py now responsible for build robots
    io.py contains new method to ensure no repeat item_ids betrween items and robots.

--- cairo_lfd/src/cairo_lfd/data/io.py
+++ cairo_lfd/src/cairo_lfd/data/io.py
@@ -14,7 +14,7 @@ class ConfigurationError(Exception):
     def __init__(self, value):
         self.value = value
 
-    def __str__(self): 
+    def __str__(self):
         return(repr(self.value))
 
 
@@ -25,13 +25,18 @@ def load_lfd_configuration(filepath):
     with open(filepath) as json_data:
         configs = json.load(json_data, object_pairs_hook=OrderedDict)
         if "constraints" not in configs:
-            raise ConfigurationError("config.json file must contain the 'constraints' key, even if its value is an empty list.")
+            raise ConfigurationError(
+                "config.json file must contain the 'constraints' key, even if its value is an empty list.")
         if "items" not in configs:
-            raise ConfigurationError("config.json file must contain the 'items' key, even if its value is an empty list.")
+            raise ConfigurationError(
+                "config.json file must contain the 'items' key, even if its value is an empty list.")
         if "robots" not in configs:
-            raise ConfigurationError("config.json file must contain the 'robots' key, even if its value is an empty list.")
+            raise ConfigurationError(
+                "config.json file must contain the 'robots' key, even if its value is an empty list.")
         if "triggers" not in configs:
-            raise ConfigurationError("config.json file must contain the 'triggers' key, even if its value is an empty list.")
+            raise ConfigurationError(
+                "config.json file must contain the 'triggers' key, even if its value is an empty list.")
+        validate_unique_item_ids(configs['robots'], configs['items'])
         return configs
 
 
@@ -51,7 +56,6 @@ def export_to_json(path, data):
 
 
 def import_csv_to_list(path, exclude_header=True):
-
     """
     Import trajectories stored as .csv files into a list of trajectories. In this case, each file represents
     a single trajectory/demonstration.
@@ -89,7 +93,6 @@ def import_csv_to_list(path, exclude_header=True):
 
 
 def import_csv_to_dict(path):
-
     """
     Import trajectories stored as .csv files into a Ordered Dictionary. In this case, each file represents
     a single trajectory/demonstration.
@@ -129,7 +132,6 @@ def import_csv_to_dict(path):
 
 
 def import_json_to_dict(path):
-
     """
     Import trajectories stored as .json files into a Ordered Dictionary. In this case, each file represents
     a single trajectory/demonstration.
@@ -163,7 +165,6 @@ def import_json_to_dict(path):
 
 
 def load_json_files(path):
-
     """
     Import JSON files as a Python dictionary from .json files in the directory signified by the path..
 
@@ -193,7 +194,6 @@ def load_json_files(path):
 
 
 def load_json_file(path):
-
     """
     Import JSON file as a Python dictionary.
 
@@ -211,3 +211,19 @@ def load_json_file(path):
     with open(path, 'r') as f:
         datastore = json.load(f)
         return datastore
+
+
+def validate_unique_item_ids(robots, items):
+    print(robots)
+    print(items)
+    ids = []
+    for robot in robots:
+        if robot['init_args']['item_id'] in ids:
+            raise ValueError("Robots and Items must have unique item ids!")
+        else:
+            ids.append(robot['init_args']['item_id'])
+    for item in items:
+        if item['init_args']['item_id'] in ids:
+            raise ValueError("Robots and Items must have unique item ids!")
+        else:
+            ids.append(item['init_args']['item_id'])

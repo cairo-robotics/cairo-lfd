45a2dba5d2f9e597a564346049b795d98dde4855
Carl <cmuell89@gmail.com>
2022-07-12 16:06:42 -0600

    Removing arguments that are no longer used

--- cairo_lfd/nodes/arpolfd_middleware.py
+++ cairo_lfd/nodes/arpolfd_middleware.py
@@ -11,7 +11,7 @@ from geometry_msgs.msg import Quaternion, Vector3
 def main():
     rospy.init_node("arpolfd_middleware")
     vrpn_transform = VRPNFixedTransform( vrpn_world_frame="optitrack_world", global_world_frame="world", translation=Vector3(0, 0, 0), rotation=Quaternion(0, 0, 0, 1))
-    mm = ARPOLfDMiddleware(ar_pos_transform=Vector3(0.975, -0.09, -0.27), ar_quat_transform=Quaternion(0.0, 0.0, 1.0, 0.0), pose_target_topic='arpo_lfd/pose_target', joint_configuration_topic="arpo_lfd/joint_configuration", playback_cmd_topic="arpo_lfd/trajectory_playback_cmd", joint_trajectory_topic="arpo_lfd/joint_trajectory")
+    mm = ARPOLfDMiddleware(ar_pos_transform=Vector3(0.975, -0.09, -0.27), ar_quat_transform=Quaternion(0.0, 0.0, 1.0, 0.0), joint_configuration_topic="arpo_lfd/joint_configuration", playback_cmd_topic="arpo_lfd/trajectory_playback_cmd", joint_trajectory_topic="arpo_lfd/joint_trajectory")
     rospy.spin()
 
   
